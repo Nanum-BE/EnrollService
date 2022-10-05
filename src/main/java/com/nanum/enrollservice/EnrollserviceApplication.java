252f5caf92484fa378d80c7d1e52f5df8b57a8c1
안대현 <gch05610@gmail.com>
2022-10-05 11:28:40 +0900

    First Init

---
+++ src/main/java/com/nanum/enrollservice/EnrollserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nanum.enrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanum/enrollservice/EnrollserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanum.enrollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
