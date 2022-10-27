8f52419f9b79d0d9fa03d7886bc3342d7f2c0c06
mins00oo <alstn7223@gmail.com>
2022-10-27 16:36:27 +0900

    [#74] fix:주석

--- src/main/java/com/nanum/enrollservice/movein/presentation/MoveInController.java
+++ src/main/java/com/nanum/enrollservice/movein/presentation/MoveInController.java
@@ -30,6 +30,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Slf4j
 @Tag(name = "하우스 입주 신청", description = "하우스 입주 신청 관련 api")
 @ApiResponses(value = {
--- src/main/java/com/nanum/kafka/messagequeue/KafkaProducerImpl.java
+++ src/main/java/com/nanum/kafka/messagequeue/KafkaProducerImpl.java
@@ -10,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
